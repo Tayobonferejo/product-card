7bad5783f010a7fb8972e90594284640320d778d
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-19 22:39:17 +0100

    styling productcard

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,16 +1,25 @@
 
     import myImage from "./assets/image-product.jpg";
+    import myCart from "./assets/icon-cart.svg";
 
 
 function ProductCard() {
-    return(<div>
-        <div>
+    return(<div className="main-cart flex-cart">
+        <div className="image-container">
             <img src={myImage}></img>
         </div>
         <div>
             <p>PERFUME</p>
             <h1>Gabrielle Essence Eau De Parfum</h1>
             <p>A floral solar and voluptuous interpretation composed by Oliver Polge, Perfumer-Creator for the House of CHANEL</p>
+            <div className="flex-cart">
+                <p>$149.99</p>
+                <p>$169.99</p>
+            </div>
+            <div className="flex-cart">
+              <img src={myCart}></img>
+              <button>Add to Cart</button>  
+            </div>
         </div>
     </div>)
 }
