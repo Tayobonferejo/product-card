d24a59076cdc9a7476f99548804581d9da50828c
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-20 22:30:23 +0100

    more styling productcard

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -13,8 +13,8 @@ function ProductCard() {
             <h1>Gabrielle Essence Eau De Parfum</h1>
             <p className="price-content">A floral solar and voluptuous interpretation composed by Oliver Polge, Perfumer-Creator for the House of CHANEL</p>
             <div className="flex-cart">
-                <p>$149.99</p>
-                <p>$169.99</p>
+                <p className="price">$149.99</p>
+                <p className="price main-price">$169.99</p>
             </div>
             <div className="flex-cart button-container">
               <img src={myCart}></img>
